d7cbc22db3bfae8a2f74acb0bdcdfc5526e6a613
Paulohfreire <paulo.freire53@gmail.com>
2020-09-20 00:20:47 -0300

    add strapi

---
+++ react-avancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd349f66d62442fa8119148b9b3913a14'),
+    },
+  },
+});
---
+++ react-avancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9336612-59b6-4665-9d52-515433bafd1d'
+};
